f7251a9c41e26a3005b34f83457856ef378ca7f8
Wojtek Czerwinski <76266906+Aenvis@users.noreply.github.com>
2024-01-05 00:28:18 +0100

    najciezsze 1.5h mojego zycia (#7)

--- LandMine.gd
+++ LandMine.gd
@@ -1,5 +1,12 @@
 extends Node3D
 
+signal player_exploded
+
 func _on_body_entered(body):
-	print("this landmine just put you under ground :P")
-	body.position.y=-50
+	#print("this landmine just put you under ground :P")
+	print("this landmine put you in the sky and killed you, maybe explosion?")
+	body.position.y=+30
+	# this signal doesn't work at all, but it should force game to end
+	player_exploded.emit()
+	
+	
