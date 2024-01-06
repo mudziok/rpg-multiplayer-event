68cbb3fe09e2871dd4bc0acfaaaa98018e290d95
kris659 <krzysztof.pedzich712@gmail.com>
2024-01-06 23:01:46 +0100

    Saving highscore, Sky material, improved gameover ui

--- internal/scenes/GameOverScreen.gd
+++ internal/scenes/GameOverScreen.gd
@@ -1,9 +1,31 @@
 extends Panel
 
 
+var distanceText
+var previousBestDistanceText
+var minutesText
+var secondsText
+var previousBestTimeText
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	distanceText = $Distance
+	previousBestDistanceText = $PreviousBestDistance
+	minutesText = $Minutes
+	secondsText = $Seconds
+	previousBestTimeText = $PreviousBestTime
+
+func game_over(distance, time, bestDistance, bestTime):
+	distanceText.text = "%02.2f" % distance
+	previousBestDistanceText.text = "best: %02.2f" % bestDistance
+	
+	var minutes = fmod(time, 3600) / 60
+	var seconds = fmod(time, 60)
+	minutesText.text = "%02d:" % minutes
+	secondsText.text = "%02d" % seconds
+	minutes = fmod(bestTime, 3600) / 60
+	seconds = fmod(bestTime, 60)
+	previousBestTimeText.text = "best: %02d:" % minutes + "%02d" % seconds
 
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://internal/code/main_screen.tscn")
