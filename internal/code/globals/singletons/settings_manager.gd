7bbbc3f4061e0713691807a26523387248e69eb9
Pojemnik <andriej194@gmail.com>
2024-01-08 20:33:20 +0100

    Add muting music

--- internal/code/globals/singletons/settings_manager.gd
+++ internal/code/globals/singletons/settings_manager.gd
@@ -8,8 +8,8 @@ func load_settings():
 	if ResourceLoader.exists(file_name):
 		var settings_data = ResourceLoader.load(file_name)
 		if settings_data is SettingsData: # Check that the data is valid
-			AudioServer.set_bus_volume_db(music_bus, settings_data.music_volume)
-			AudioServer.set_bus_volume_db(sound_bus, settings_data.sound_volume)
+			_set_music_volume(settings_data.music_volume)
+			_set_sound_volume(settings_data.sound_volume)
 
 func _save_settings():
 	var settings_data = SettingsData.new()
@@ -23,16 +23,18 @@ func set_music_volume(value):
 	
 func _set_music_volume(value):
 	AudioServer.set_bus_volume_db(music_bus, value)
+	AudioServer.set_bus_mute(music_bus, value == -30)
 	
 func get_music_volume():
 	return AudioServer.get_bus_volume_db(music_bus)
 	
 func set_sound_volume(value):
-	_set_music_volume(value)
+	_set_sound_volume(value)
 	_save_settings()
 	
 func _set_sound_volume(value):
 	AudioServer.set_bus_volume_db(sound_bus, value)
+	AudioServer.set_bus_mute(sound_bus, value == -30)
 	
 func get_sound_volume():
 	return AudioServer.get_bus_volume_db(sound_bus)
