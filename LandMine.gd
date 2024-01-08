68b84df411cacbaf8edd85ae80525100557ba756
Pojemnik <andriej194@gmail.com>
2024-01-08 22:02:00 +0100

    Make mines add velocity instead of teleporting

--- LandMine.gd
+++ LandMine.gd
@@ -3,10 +3,9 @@ extends Node3D
 signal player_exploded
 
 func _on_body_entered(body):
-	#print("this landmine just put you under ground :P")
-	print("this landmine put you in the sky and killed you, maybe explosion?")
-	body.position.y=+30
+	if "velocity" in body:
+		print("this landmine put you in the sky and killed you, maybe explosion?")
+		body.velocity.y += 20
 	# this signal doesn't work at all, but it should force game to end
+	# maybe it's better this way?
 	player_exploded.emit()
-	
-	
