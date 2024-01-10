f1411426f8e1aa8c8545592a3246e6bc137d29ba
Poleksiak <150528813+Poleksiak@users.noreply.github.com>
2024-01-10 21:45:43 +0100

    Day 12 - Poleksiak

--- internal/scenes/game.gd
+++ internal/scenes/game.gd
@@ -9,9 +9,20 @@ var map_generator
 var gameover_screen
 var speedometer
 var test
+var kite
+var kite_material
 var file_name = "user://test_save.tres"
 var bestDistance = 0
 var bestTime = 0
+
+var great_text_variation = ['[center]GREAT![/center]', '[center]ZOOMING FAST![/center]', '[center]FAST BOI![/center]',
+'[center]FASTER THAN A LIGHT![/center]', '[center]AAAAAAA![/center]']
+var jump_text_variation = ['[center]NICE JUMP![/center]', '[center]FLYING HIIIIGH![/center]', '[center]IS IT BIRD OR PLANE?![/center]',
+'[center]DONT TRY THIS AT HOME![/center]', '[center]LIKE A BIRD IN THE SKY[/center]']
+var is_great_text_set = false
+var is_jump_text_set = false
+var growing_great_text = true
+var growing_jump_text = true
 
 func _ready():
 	time = 0.0
@@ -19,6 +30,8 @@ func _ready():
 	map_generator = $map_controler
 	gameover_screen = $GameOverScreen
 	speedometer = $speedometer
+	kite = $Player/Kite
+	prepare_color_changing()
 
 # update timer every milisecond
 func update_score(delta):
@@ -28,13 +41,15 @@ func update_score(delta):
 	$Seconds.text = "%02d" % seconds
 
 func _process(delta):
+	change_kite_color()
+	show_great_texts(delta)
 	update_score(delta)
-	
-	
+
 func _on_player_on_player_lose():
 	#może po jakimś delayu?
 	set_ui_invisible()
 	gameover_screen.visible = true
+	$ColorChangeScreen.visible = true
 	load_data()
 	gameover_screen.game_over(speedometer.distanceTraveled, time, bestDistance, bestTime)
 	if(speedometer.distanceTraveled > bestDistance):
@@ -69,4 +84,69 @@ func set_ui_invisible():
 	$Minutes.visible = false
 	$Seconds.visible = false
 	$UIBackground.visible = false
+	$ColorChangeScreen.visible = false
+	$GreatTextLabel.visible = false
+	$NiceJumpLabel.visible = false
+	
+func prepare_color_changing():
+	kite_material = kite.get_active_material(0)
+	kite_material.albedo_color = CustomizationOptions.kite_color
+	$ColorChangeScreen/ColorSliders/RedSlider.set_value(CustomizationOptions.red)
+	$ColorChangeScreen/ColorSliders/GreenSlider.set_value(CustomizationOptions.green)
+	$ColorChangeScreen/ColorSliders/BlueSlider.set_value(CustomizationOptions.blue)
+	
+func change_kite_color():
+	CustomizationOptions.red = $ColorChangeScreen/ColorSliders/RedSlider.get_value()
+	CustomizationOptions.green = $ColorChangeScreen/ColorSliders/GreenSlider.get_value()
+	CustomizationOptions.blue = $ColorChangeScreen/ColorSliders/BlueSlider.get_value()
+	CustomizationOptions.update_color()
+	kite_material.albedo_color = CustomizationOptions.kite_color
 
+func show_great_texts(delta):
+	if speedometer.velocity > 5.0:
+		if not is_great_text_set:
+			$GreatTextLabel.scale = Vector2(1, 1)
+			$GreatTextLabel.position = Vector2(randi() % 800 + 50, randi() % 550 + 50)
+			$GreatTextLabel.clear()
+			$GreatTextLabel.push_color(Color(randf(), randf(), randf()))
+			$GreatTextLabel.append_text(great_text_variation[randi() % great_text_variation.size()])
+			$GreatTextLabel.pop()
+			$GreatTextLabel.visible = true
+			is_great_text_set = true
+			
+		if growing_great_text:
+			$GreatTextLabel.scale += Vector2(delta * 0.75, delta * 0.75)
+		else:
+			$GreatTextLabel.scale -= Vector2(delta * 0.75, delta * 0.75)
+			
+		if $GreatTextLabel.scale.x >= 1.5:
+			growing_great_text = false
+		elif $GreatTextLabel.scale.x <= 0.5:
+			growing_great_text = true
+	else:
+		$GreatTextLabel.visible = false
+		is_great_text_set = false
+	if abs($Player/Surfer.velocity.y) > 0:
+		if not is_jump_text_set:
+			$NiceJumpLabel.scale = Vector2(1, 1)
+			$NiceJumpLabel.position = Vector2(randi() % 800 + 50, randi() % 550 + 50)
+			$NiceJumpLabel.clear()
+			$NiceJumpLabel.push_color(Color(randf(), randf(), randf()))
+			$NiceJumpLabel.append_text(jump_text_variation[randi() % jump_text_variation.size()] )
+			$NiceJumpLabel.pop()
+			$NiceJumpLabel.visible = true
+			is_jump_text_set = true
+		
+		if growing_jump_text:
+			$NiceJumpLabel.scale += Vector2(delta * 0.75, delta * 0.75)
+		else:
+			$NiceJumpLabel.scale -= Vector2(delta * 0.75, delta * 0.75)
+			
+		if $NiceJumpLabel.scale.x >= 1.5:
+			growing_jump_text = false
+		elif $NiceJumpLabel.scale.x <= 0.5:
+			growing_jump_text = true
+	else:
+		$NiceJumpLabel.visible = false
+		is_jump_text_set = false
+	
--- internal/speedometer/speedometer.gd
+++ internal/speedometer/speedometer.gd
@@ -5,6 +5,7 @@ signal provide_velocity(velocity: float)
 var last_position:Vector3
 var distanceTraveled: float = 0
 var delta_time:float=0
+var velocity
 
 func _process(delta):
 	delta_time=delta;
@@ -15,7 +16,7 @@ func _on_player_provide_position(pose):
 	last_position=pose
 	displace.y=0
 	displace.x=0
-	var velocity = (displace.length_squared()/delta_time)
+	velocity = (displace.length_squared()/delta_time)
 	distanceTraveled += displace.length_squared()
 	$VelocityNumber.text = "Velocity: %02.2f" % velocity
 	$DistanceTraveledNumber.text = "Distance traveled: %02.2f" % distanceTraveled
